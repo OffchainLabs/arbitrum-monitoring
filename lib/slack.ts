f967815f90d33d3942ecc6b0890386f0f57cee8a
dewanshparashar <dewanshparashar@yahoo.in>
2024-06-04 12:36:22 +0400

    Dev: updated monitoring token names

--- lib/slack.ts
+++ lib/slack.ts
@@ -2,8 +2,9 @@ import { WebClient } from '@slack/web-api'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const slackToken = process.env.SLACK_TOKEN
-const slackChannel = process.env.SLACK_CHANNEL || 'C02R00X0HLN'
+const slackToken = process.env.ORBIT_RETRYABLE_MONITORING_SLACK_TOKEN
+const slackChannel =
+  process.env.ORBIT_RETRYABLE_MONITORING_SLACK_CHANNEL || 'C02R00X0HLN'
 
 const web = new WebClient(slackToken)
 export const slackMessageRetryablesMonitor = (text: string) => {
