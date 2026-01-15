d095677220a12ee99521e5211fa2e1f7f8b3b0fb
Dewansh <dewanshparashar@yahoo.in>
2026-01-15 16:26:28 +0400

    Revert "refactor: deduplicate Slack posting logic with createSlackPoster fact…"
    This reverts commit a884d1075ed1520548678086fb5856e949407aeb.

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -2,7 +2,6 @@ export * from './types'
 export * from './config'
 export { getExplorerUrlPrefixes } from './getExplorerUrlPrefixes'
 export { postSlackMessage } from './postSlackMessage'
-export { createSlackPoster } from './createSlackPoster'
 
 export const sleep = (ms: number) =>
   new Promise(resolve => setTimeout(resolve, ms))
