4df4e46303fe7dd9b7196a57ba451c9ef647f538
Dewansh <dparashar@offchainlabs.com>
2026-01-15 16:28:35 +0400

    Revert "refactor: deduplicate Slack posting logic with createSlackPoster fact…" (#89)
    This reverts commit a884d1075ed1520548678086fb5856e949407aeb.

--- packages/retryable-monitor/handlers/slack/postSlackMessage.ts
+++ packages/retryable-monitor/handlers/slack/postSlackMessage.ts
@@ -1,6 +1,18 @@
-import { createSlackPoster } from 'utils'
+import { postSlackMessage as commonPostSlackMessage } from '../../../utils/postSlackMessage'
 
-export const postSlackMessage = createSlackPoster({
-  tokenEnvVar: 'RETRYABLE_MONITORING_SLACK_TOKEN',
-  channelEnvVar: 'RETRYABLE_MONITORING_SLACK_CHANNEL',
-})
+const slackToken = process.env.RETRYABLE_MONITORING_SLACK_TOKEN
+const slackChannel = process.env.RETRYABLE_MONITORING_SLACK_CHANNEL
+
+export const postSlackMessage = ({ message }: { message: string }) => {
+  if (!slackToken) throw new Error(`Slack token is required.`)
+  if (!slackChannel) throw new Error(`Slack channel is required.`)
+
+  if (process.env.NODE_ENV === 'DEV') return
+  if (process.env.NODE_ENV === 'CI' && message === 'success') return
+
+  commonPostSlackMessage({
+    slackToken,
+    slackChannel,
+    message,
+  })
+}
